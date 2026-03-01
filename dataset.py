f7e79a7e6d7268887f3ea9fb74bcb4c1a2e766f3
Tran-phuong <115384550+ctrlc0704@users.noreply.github.com>
2026-03-01 13:41:50 +0000

    update dataset onilne

--- dataset.py
+++ dataset.py
@@ -1,29 +1,41 @@
 import os
-from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
+import pandas as pd
+import torch
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+from torchvision import transforms
 
-def get_transforms(train=True):
-    if train:
-        return transforms.Compose([
-            transforms.Resize((224, 224)),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-        ])
-    else:
-        return transforms.Compose([
-            transforms.Resize((224, 224)),
-            transforms.ToTensor(),
-        ])
+class ChestXrayDataset(Dataset):
+    def __init__(self, csv_file, img_dir, transform=None):
+        self.df = pd.read_csv(csv_file)
+        self.img_dir = img_dir
+        self.transform = transform
 
-def get_dataloader(data_dir, batch_size=32, train=True):
-    dataset = datasets.ImageFolder(
-        root=data_dir,
-        transform=get_transforms(train)
-    )
-    loader = DataLoader(
-        dataset,
-        batch_size=batch_size,
-        shuffle=train,
-        num_workers=2
-    )
-    return loader, len(dataset.classes)
+        self.labels = self.df.iloc[:, 1:].values.astype("float32")
+        self.image_names = self.df.iloc[:, 0].values
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.image_names[idx])
+        image = Image.open(img_path).convert("RGB")
+
+        if self.transform:
+            image = self.transform(image)
+
+        label = torch.tensor(self.labels[idx])
+
+        return image, label
+
+
+def get_chestxray_loader(csv_file, img_dir, batch_size=32):
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+    ])
+
+    dataset = ChestXrayDataset(csv_file, img_dir, transform)
+    loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+
+    return loader
